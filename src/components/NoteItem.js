696572a833bfd85cf96f8d6e3ded3bc1cc2db0fe
Mandar Deshpande <mdman2257@gmail.com>
2022-04-20 09:26:54 +0530

    css changes

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -13,8 +13,8 @@ function NoteItem(props) {
           <div className="card-body">
             <h5 className="card-title">{note.title}</h5>
             <p className="card-text">{note.description}</p>
-            <i className="fa-solid fa-pen-to-square fa-lg mx-2" style={{ color: "blue" }}></i>
-            <i className="fa-solid fa-trash-can fa-lg mx-2" style={{ color: "red" }}></i>
+            <i className="fa-solid fa-pen-to-square fa-lg mx-2"></i>
+            <i className="fa-solid fa-trash-can fa-lg mx-2"></i>
           </div>
         </div>
       </div>
